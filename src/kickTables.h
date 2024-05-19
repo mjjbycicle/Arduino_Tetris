ca82915b8a4811aee9f098ecb8f36525639da616
Michael Jiang <nengzhimj@gmail.com>
2024-05-19 18:57:31 -0500

    kick tables

---
+++ src/kickTables.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "Vec.h"
+
+vec
+        upToRightKickData[]{
+        vec(0, 0),
+        vec(-1, 0),
+        vec(-1, 1),
+        vec(0, -2),
+        vec(-1, -2)
+},
+        rightToUpKickData[]{
+        vec(0, 0),
+        vec(1, 0),
+        vec(1, -1),
+        vec(0, 2),
+        vec(1, 2)
+},
+        rightToDownKickData[]{
+        vec(0, 0),
+        vec(1, 0),
+        vec(1, -1),
+        vec(0, -2),
+        vec(1, 2)
+},
+        downToRightKickData[]{
+        vec(0, 0),
+        vec(-1, 0),
+        vec(-1, 1),
+        vec(0, 2),
+        vec(-1, -2)
+},
+        downToLeftKickData[]{
+        vec(0, 0),
+        vec(1, 0),
+        vec(1, 1),
+        vec(0, -2),
+        vec(1, -2)
+},
+        leftToDownKickData[]{
+        vec(0, 0),
+        vec(-1, 0),
+        vec(-1, -1),
+        vec(0, 2),
+        vec(-1, 2)
+},
+        leftToUpKickData[]{
+        vec(0, 0),
+        vec(-1, 0),
+        vec(-1, -1),
+        vec(0, 2),
+        vec(-1, 2)
+},
+        upToLeftKickData[]{
+        vec(0, 0),
+        vec(1, 0),
+        vec(1, 1),
+        vec(0, -2),
+        vec(1, -2)
+};
